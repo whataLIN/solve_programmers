13ba0d36d4d0a5d4b35fc39a4625cef4feceb346
whataLIN <102681611+whataLIN@users.noreply.github.com>
2023-04-19 10:28:57 +0900

    java 폴더 추가

---
+++ java/lv.0/230419_숫자비교.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int solution(int num1, int num2) {
+        int answer=(num1==num2)?1:-1;
+        return answer;;
+    }
+}
+
+
+// 삼항연산자 오랜만
+// 문법 기억이 안난다; 자꾸 세미콜론 빼먹지마라
