c5afd0402992afcd26e2838e395ef5a2824eb9a5
whataLIN <102681611+whataLIN@users.noreply.github.com>
2023-04-19 16:45:21 +0900

    230419

---
+++ java/lv.0/230419_나머지구하기.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int solution(int num1, int num2) {
+        int answer = num1%num2;
+        return answer;
+    }
+}
+
+//혹은
+
+class Solution {
+    public int solution(int num1, int num2) {
+        return num1%num2;
+    }
+}
